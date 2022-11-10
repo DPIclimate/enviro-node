6f8a9ca53b968b2405a40e1c9cbe0c22361e692b
Harvey Bates <phenobottle@gmail.com>
2022-11-11 09:17:56 +1100

    first efforts with the new esp32 motherboard version

--- firmware/tests/esp32_debugger/src/main.cpp
+++ firmware/tests/esp32_debugger/src/main.cpp
@@ -1,8 +1,40 @@
 #include <Arduino.h>
+
 void setup() {
-// write your initialization code here
+    pinMode(GPIO_NUM_17, OUTPUT);
+}
+
+void startup_tone(){
+    tone(GPIO_NUM_17, 494); // B4
+    delay(100);
+    tone(GPIO_NUM_17, 740); // F5#
+    delay(100);
+    tone(GPIO_NUM_17, 784); // G5
+    delay(100);
+    tone(GPIO_NUM_17, 1175); // D6
+    delay(400);
+    noTone(GPIO_NUM_17);
+}
+
+void shutdown_tone(){
+    tone(GPIO_NUM_17, 1175); // D6
+    delay(100);
+    tone(GPIO_NUM_17, 784); // G5
+    delay(100);
+    tone(GPIO_NUM_17, 740); // F5#
+    delay(100);
+    tone(GPIO_NUM_17, 494); // B4
+    delay(400);
+    noTone(GPIO_NUM_17);
+}
+
+void command_tone(){
+    tone(GPIO_NUM_17, 1319); // E6
+    delay(200);
+    noTone(GPIO_NUM_17);
 }
 
 void loop() {
-// write your code here
+    //startup_tone();
+    delay(60000);
 }
