db455c4bee59c9e75fb96c49fa432c308cf8926c
Harvey Bates <phenobottle@gmail.com>
2022-11-11 10:45:51 +1100

    added TCA9534 compatability for 12 V output

--- firmware/tests/esp32_debugger/src/main.cpp
+++ firmware/tests/esp32_debugger/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <Wire.h>
+
 #include "audio_feedback.h"
+#include "TCA9534.h"
+
+TCA9534 io_expander;
 
 void setup() {
     init_tones();
-    startup_tone();
+
+    Serial.begin(115200);
+    while(!Serial) delay(1);
+
+    //startup_tone();
+
+    Wire.begin(GPIO_NUM_25, GPIO_NUM_23);
+    io_expander.attach(Wire);
+    io_expander.setDeviceAddress(0x20);
+
+    io_expander.config(TCA9534::Config::OUT);
+    io_expander.config(4, TCA9534::Config::IN);
+    io_expander.output(1, TCA9534::Level::H);
 }
 
 void loop() {
