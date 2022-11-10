62649a29928dc5e896bce312e6d61f9529c2ebfa
Harvey Bates <phenobottle@gmail.com>
2022-11-11 09:47:37 +1100

    added audio_feedback through piezo

--- firmware/tests/esp32_debugger/src/main.cpp
+++ firmware/tests/esp32_debugger/src/main.cpp
@@ -1,40 +1,11 @@
 #include <Arduino.h>
+#include "audio_feedback.h"
 
 void setup() {
     pinMode(GPIO_NUM_17, OUTPUT);
 }
 
-void startup_tone(){
-    tone(GPIO_NUM_17, 494); // B4
-    delay(100);
-    tone(GPIO_NUM_17, 740); // F5#
-    delay(100);
-    tone(GPIO_NUM_17, 784); // G5
-    delay(100);
-    tone(GPIO_NUM_17, 1175); // D6
-    delay(400);
-    noTone(GPIO_NUM_17);
-}
-
-void shutdown_tone(){
-    tone(GPIO_NUM_17, 1175); // D6
-    delay(100);
-    tone(GPIO_NUM_17, 784); // G5
-    delay(100);
-    tone(GPIO_NUM_17, 740); // F5#
-    delay(100);
-    tone(GPIO_NUM_17, 494); // B4
-    delay(400);
-    noTone(GPIO_NUM_17);
-}
-
-void command_tone(){
-    tone(GPIO_NUM_17, 1319); // E6
-    delay(200);
-    noTone(GPIO_NUM_17);
-}
-
 void loop() {
-    //startup_tone();
+    startup_tone();
     delay(60000);
 }
