502d55b4bdc6115704261991445d572b063c8f74
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2023-03-20 14:56:26 +1100

    Added a hack to reduce the changes required to get ulptool-pio working.

--- firmware/wombat/gen_version_h.py
+++ firmware/wombat/gen_version_h.py
@@ -1,4 +1,5 @@
 import os, subprocess
+from pathlib import Path
 
 # Import the current working construction
 # environment to the `env` variable.
@@ -6,6 +7,29 @@ import os, subprocess
 Import('env')
 
 print('====== START PRE SCRIPT')
+
+# This code to is make the ulptool-pio scripts work. They must be written against an older
+# version of the ESP32 tools and expect things to have different names and locations. A simple
+# way to fix one of those errors is to copy the sdkconfig.h file to where ulptool-pio expects
+# it to be.
+
+# A different problem cannot be fixed this easily and had to be fixed in a ulptool-pio python
+# script which is why we have our own fork of it.
+
+# In ~/.platformio/packages/framework-arduinoespressif32/tools/sdk:
+# mkdir -p include/config
+# cp esp32/qout_qspi/include/sdkconfig.h include/config
+
+platform = env.PioPlatform()
+framework_dir = platform.get_package_dir("framework-arduinoespressif32")
+to_path = Path(framework_dir, 'tools/sdk/include/config')
+from_path = Path(framework_dir, f'tools/sdk/{env["BOARD_MCU"]}/qout_qspi/include/sdkconfig.h')
+
+if from_path.exists():
+    to_path.mkdir(parents=True, exist_ok=True)
+    result = subprocess.run(['cp', str(from_path), str(to_path)], capture_output=True, check=True)
+
+# End of ulptool-pio hack
 
 _VERSION_H = 'include/version.h'
 
