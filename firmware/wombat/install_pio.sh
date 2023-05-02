ebe69dcb5e982a3abfc48e8d574e3957b3a6222b
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2023-05-02 11:49:13 +1000

    Added a script to install platformio and the wombat variant files.

---
+++ firmware/wombat/install_pio.sh
@@ -0,0 +1,23 @@
+set -x
+cd $(dirname $0)
+# Install platformio core.
+python3.10 -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+
+# This needs to be in a login script too.
+PATH="~/.platformio/penv/bin:$PATH"
+
+# This command will fail due to the lack of a dpiclimate_esp_32_wombat.json file. Copying the file before running
+# the command does not work - the directory must get cleared or something.
+pio pkg install -p platformio/espressif32
+
+# Now copy the missing file
+cp pio/platforms/espressif32/boards/dpiclimate_esp32_wombat.json ~/.platformio/platforms/espressif32/boards
+
+# Run the command again and it will complete.
+pio pkg install -p platformio/espressif32
+
+# Copy the variant pins file over to complete the ESP32 Wombat installation.
+mkdir -p ~/.platformio/packages/framework-arduinoespressif32/variants/dpiclimate_esp32_wombat
+cp pio/packages/framework-arduinoespressif32/variants/dpiclimate_esp32_wombat/pins_arduino.h ~/.platformio/packages/framework-arduinoespressif32/variants/dpiclimate_esp32_wombat
+
+# Now pio run should work.
