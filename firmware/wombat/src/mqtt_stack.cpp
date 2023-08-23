16a462db3f47249dd4bc049067747796bce6fdbd
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2023-08-23 14:06:29 +1000

    Changed uplinks.cpp to allow longer messages, refactored the file processing so the file is only opened, closed, and deleted in the process_file function.

--- firmware/wombat/src/mqtt_stack.cpp
+++ firmware/wombat/src/mqtt_stack.cpp
@@ -10,9 +10,6 @@
 #define MAX_RSP 64
 static char rsp[MAX_RSP + 1];
 
-#define MAX_BUF 2048
-static char buf[MAX_BUF + 1];
-
 // This buffer is allocated in the mqtt_login method but the script should be
 // run later, so it must be freed elsewhere after the run.
 char *script = nullptr;
@@ -60,6 +57,7 @@ void mqttCmdCallback(int cmd, int result) {
             break;
         case SARA_R5_MQTT_COMMAND_PUBLISH:
         case SARA_R5_MQTT_COMMAND_PUBLISHBINARY:
+        case SARA_R5_MQTT_COMMAND_PUBLISHFILE:
             mqttPublishOk = (result == 1);
             break;
     }
@@ -123,55 +121,63 @@ bool mqtt_login(void) {
         String cmd_topic("wombat/");
         cmd_topic += config.node_id;
         r5.subscribeMQTTtopic(1, cmd_topic);
-        mqttGotURC = false;
 
         ESP_LOGI(TAG, "Checking for a config script, waiting for subscribe URC");
-        while (!mqttGotURC) {
-            delay(20);
+        lastCmd = SARA_R5_MQTT_COMMAND_INVALID;
+        lastResult = 0;
+        mqttGotURC = false;
+        int attempts = 30;
+        while (!mqttGotURC && attempts > 0) {
+            delay(100);
             r5.bufferedPoll();
+            attempts--;
         }
 
-        // FIXME: What if the URC above is an error?
-
-        for (int i = 0; i < 50; i++) {
-            delay(20);
-            mqttGotURC = false;
-            r5.bufferedPoll();
-            if (mqttGotURC && lastCmd == SARA_R5_MQTT_COMMAND_READ) {
-                break;
-            }
-        }
-
-        if (lastCmd == SARA_R5_MQTT_COMMAND_READ && lastResult > 0) {
-            ESP_LOGI(TAG, "Config download waiting.");
-            int qos;
-            String topic;
-            int bytes_read = 0;
-            memset(g_buffer, 0, MAX_G_BUFFER + 1);
-            SARA_R5_error_t err = r5.readMQTT(&qos, &topic, (uint8_t *) g_buffer, MAX_G_BUFFER, &bytes_read);
-            if (bytes_read > 0) {
-                // Publish a zero length message to clear the retained message.
-                r5.mqttPublishTextMsg(cmd_topic, "", 0, true);
-                uint8_t counter = 0;
+        if (lastCmd == SARA_R5_MQTT_COMMAND_SUBSCRIBE && lastResult != 0) {
+            lastCmd = SARA_R5_MQTT_COMMAND_INVALID;
+            lastResult = 0;
+            for (int i = 0; i < 50; i++) {
+                delay(20);
                 mqttGotURC = false;
-                while (mqttGotURC != true && counter < 50) {
-                    r5.bufferedPoll();
-                    delay(20);
-                    counter++;
+                r5.bufferedPoll();
+                if (mqttGotURC && lastCmd == SARA_R5_MQTT_COMMAND_READ) {
+                    break;
                 }
+            }
 
-                script = static_cast<char *>(malloc(bytes_read + 1));
-                if (script != nullptr) {
-                    memcpy(script, g_buffer, bytes_read);
-                    script[bytes_read] = 0;
+            if (lastCmd == SARA_R5_MQTT_COMMAND_READ && lastResult > 0) {
+                ESP_LOGI(TAG, "Config download waiting.");
+                int qos;
+                String topic;
+                int bytes_read = 0;
+                memset(g_buffer, 0, MAX_G_BUFFER + 1);
+                SARA_R5_error_t err = r5.readMQTT(&qos, &topic, (uint8_t *) g_buffer, MAX_G_BUFFER, &bytes_read);
+                if (bytes_read > 0) {
+                    // Publish a zero length message to clear the retained message.
+                    r5.mqttPublishTextMsg(cmd_topic, "", 0, true);
+                    uint8_t counter = 0;
+                    mqttGotURC = false;
+                    while (mqttGotURC != true && counter < 50) {
+                        r5.bufferedPoll();
+                        delay(20);
+                        counter++;
+                    }
+
+                    script = static_cast<char *>(malloc(bytes_read + 1));
+                    if (script != nullptr) {
+                        memcpy(script, g_buffer, bytes_read);
+                        script[bytes_read] = 0;
+                    } else {
+                        ESP_LOGE(TAG, "Could not allocate memory for script");
+                    }
                 } else {
-                    ESP_LOGE(TAG, "Could not allocate memory for script");
+                    ESP_LOGW(TAG, "Did not read any config data.");
                 }
             } else {
-                ESP_LOGW(TAG, "Did not read any config data.");
+                ESP_LOGI(TAG, "No config script");
             }
         } else {
-            ESP_LOGI(TAG, "No config script");
+            ESP_LOGE(TAG, "MQTT subscribe failed");
         }
     } else {
         ESP_LOGI(TAG, "Already have a config script, not checking for a new one");
@@ -195,22 +201,22 @@ bool mqtt_logout(void) {
 bool mqtt_publish(String &topic, const char *const msg, size_t msg_len) {
     SARA_R5_error_t err = SARA_R5_ERROR_INVALID;
     if (msg_len < MAX_MQTT_DIRECT_MSG_LEN) {
-        ESP_LOGI(TAG, "Direct publish message: %s/%s", topic.c_str(), msg);
+        ESP_LOGD(TAG, "Direct publish message: %s/%s", topic.c_str(), msg);
         err = r5.mqttPublishBinaryMsg(topic, msg, msg_len, 1);
     } else {
-        ESP_LOGI(TAG, "Publish from file: %s/%s", topic.c_str(), msg);
+        ESP_LOGD(TAG, "Publish from file: %s/%s", topic.c_str(), msg);
         static const String mqtt_msg_filename("mqtt.json");
         r5.deleteFile(mqtt_msg_filename);
         r5.appendFileContents(mqtt_msg_filename, msg, static_cast<int>(msg_len));
         memset(g_buffer, 0, sizeof(g_buffer));
-        err = r5.getFileContents(mqtt_msg_filename, g_buffer);
-        ESP_LOGD(TAG, "Msg content from modem:\n%s", g_buffer);
+        err = r5.mqttPublishFromFile(topic, mqtt_msg_filename);
 
-        if (err == SARA_R5_error_t::SARA_R5_ERROR_SUCCESS) {
-            err = r5.mqttPublishFromFile(topic, mqtt_msg_filename);
-        } else {
-            ESP_LOGE(TAG, "Failed to read file content from R5");
-        }
+//        err = r5.getFileContents(mqtt_msg_filename, g_buffer);
+//        if (err == SARA_R5_error_t::SARA_R5_ERROR_SUCCESS) {
+//            ESP_LOGD(TAG, "Msg content from modem:\n%s", g_buffer);
+//        } else {
+//            ESP_LOGE(TAG, "Failed to read file content from R5");
+//        }
     }
 
     if (err != SARA_R5_error_t::SARA_R5_ERROR_SUCCESS) {
--- firmware/wombat/src/uplinks.cpp
+++ firmware/wombat/src/uplinks.cpp
@@ -15,48 +15,60 @@ enum mqtt_status_t {
     MQTT_LOGIN_FAILED
 };
 
-static mqtt_status_t mqtt_status = MQTT_UNINITIALISED;
+static volatile mqtt_status_t mqtt_status = MQTT_UNINITIALISED;
 
 static String topic("wombat");
 
-static constexpr size_t MAX_MSG_LEN = 1024;
+static constexpr size_t MAX_MSG_LEN = 4096;
 static char msg_buf[MAX_MSG_LEN + 1];
 
-static constexpr size_t MAX_FILENAME_LEN = 64;
-static char msg_filename[MAX_FILENAME_LEN + 1];
+/**
+ * Sends the given file via MQTT.
+ *
+ * @param filename the file to send, including the leading '/' required by SPIFFS.
+ *
+ * @return true if the message was sent ok, otherwise false.
+ */
+static bool process_file(const String& filename) {
+    ESP_LOGI(TAG, "Processing message file [%s]", filename.c_str());
 
-static bool process_file(File& file) {
-    ESP_LOGI(TAG, "Processing message file [%s]", file.name());
+    File file = SPIFFS.open(filename);
+    // Just in case a directory shows up with a match on the filename pattern. May as well say it
+    // was processed ok.
+    if (file.isDirectory()) {
+        file.close();
+        return false;
+    }
 
     size_t len = file.available();
-    if (len > MAX_MSG_LEN) {
+    if (len < MAX_MSG_LEN) {
+        // Read and close the message file now in case there is a config script coming
+        // that wants to use SPIFFS.
+        file.readBytes(msg_buf, len);
+        msg_buf[len] = 0;
+        file.close();
+
+        if (mqtt_status == MQTT_UNINITIALISED) {
+            connect_to_internet();
+            mqtt_status = mqtt_login() ? MQTT_LOGIN_OK : MQTT_LOGIN_FAILED;
+            if (mqtt_status == MQTT_LOGIN_FAILED) {
+                ESP_LOGE(TAG, "Not processing file, no MQTT connection");
+                return false;
+            }
+        }
+
+        // This is not always true - if this function is called after a failed login then
+        // we want to skip publishing the message.
+        if (mqtt_status == MQTT_LOGIN_OK) {
+            if (mqtt_publish(topic, msg_buf, len)) {
+                SPIFFS.remove(filename);
+                return true;
+            }
+        }
+    } else {
         ESP_LOGE(TAG, "Message too long");
         file.close();
-        // This signals the message file can be deleted. If it is too long to send
-        // there is no point trying again later.
-        return true;
-
-    }
-
-    // Read and close the message file now in case there is a config script coming
-    // that wants to use SPIFFS.
-    file.readBytes(msg_buf, len);
-    msg_buf[len] = 0;
-    file.close();
-
-    if (mqtt_status == MQTT_UNINITIALISED) {
-        connect_to_internet();
-        mqtt_status = mqtt_login() ? MQTT_LOGIN_OK : MQTT_LOGIN_FAILED;
-        if (mqtt_status == MQTT_LOGIN_FAILED) {
-            ESP_LOGE(TAG, "Not processing file, no MQTT connection");
-            return false;
-        }
-    }
-
-    // This is not always true - if this function is called after a failed login then
-    // we want to skip publishing the message.
-    if (mqtt_status == MQTT_LOGIN_OK) {
-        return mqtt_publish(topic, msg_buf, len);
+        SPIFFS.remove(filename);
     }
 
     return false;
@@ -75,35 +87,18 @@ void send_messages(void) {
 
         uint16_t file_count = 0;
         uint16_t upload_errors = 0;
-        File file = root.openNextFile();
-        while (file) {
-            bool remove_file = false;
-            const char *filename = file.name();
-            // Copy the filename for the delete operation because the above pointer
-            // becomes invalid after the file is closed.
-            strncpy(msg_filename, filename, MAX_FILENAME_LEN);
-
-            if ( ! file.isDirectory()) {
-                if ( ! strncmp(msg_filename, msg_file_prefix, msg_file_prefix_len)) {
-                    if (file_count > 0) {
-                        delay(250);
-                    }
-
-                    // NOTE: process_file is responsible for closing the file.
-                    // This is due to weirdness around configs being downloaded and
-                    // saved to SPIFFS. process_file must read the message file and
-                    // close it before trying to log in to MQTT. Needs a redesign.
-                    remove_file = process_file(file);
-                    file_count++;
-                    if ( ! remove_file) {
-                        upload_errors++;
-                    }
+        String filename = root.getNextFileName();
+        while (filename.length() > 0) {
+            ESP_LOGD(TAG, "Found file %s", filename.c_str());
+            if ( ! strncmp(&filename.c_str()[1], msg_file_prefix, msg_file_prefix_len)) {
+                if (file_count > 0) {
+                    delay(250);
                 }
-            }
 
-            if (remove_file) {
-                snprintf(g_buffer, MAX_G_BUFFER, "/%s", msg_filename);
-                SPIFFS.remove(g_buffer);
+                if (!process_file(filename)) {
+                    upload_errors++;
+                }
+                file_count++;
             }
 
             if (mqtt_status == MQTT_LOGIN_FAILED) {
@@ -111,7 +106,7 @@ void send_messages(void) {
                 break;
             }
 
-            file = root.openNextFile();
+            filename = root.getNextFileName();
         }
 
         root.close();
