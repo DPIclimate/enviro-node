a92281f6f17a184f2ad9ae0258728f4abdd74660
dajtxx <dajtxx@users.noreply.github.com>
2023-11-06 02:42:40 +0000

    Deploying to gh-pages from @ DPIclimate/enviro-node@7b5b8c08f67312f4c587a83316fbbdc911a5df6d 🚀

--- cat-m1_8cpp.js
+++ cat-m1_8cpp.js
@@ -1,6 +1,7 @@
 var cat_m1_8cpp =
 [
     [ "get_response", "cat-m1_8cpp.html#aa19c2621e480abc2cda9cb0d4a23817d", null ],
+    [ "getNTPTime", "cat-m1_8cpp.html#aca46732143b5c7af5f57aa0e43ce6694", null ],
     [ "r5", "cat-m1_8cpp.html#a7f9b6ad99b4ad4d8ca1d579575944f2b", null ],
     [ "response_buffer_", "cat-m1_8cpp.html#a70111ee2fd620a603fc5873254338493", null ]
 ];
